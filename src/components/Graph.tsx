93a99a4f917690f0db7437db680e270bc501384d
Mirko Lenz <info@mirko-lenz.de>
2022-03-23 10:57:25 +0100

    Clean up context menu

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -136,108 +136,6 @@ function initEdgeHandles(
   return eh;
 }
 
-/*
- *
- * Set up context menus
- *
- * */
-// const cxtmenuOptions = {
-//   selector: "",
-//   commands: [],
-//   menuRadius: function (ele) {
-//     return 150 - 0.5 * ele.outerWidth() + 5;
-//     // radius - node size + 0.5 * spotlightRadius
-//   }, // the outer radius (node center to the end of the menu) in pixels. It is added to the rendered size of the node. Can either be a number or function as in the example.
-//   fillColor: "rgba(0, 0, 0, 0.75)", // the background colour of the menu
-//   activeFillColor: "rgba(0, 0, 255, 0.75)", // the colour used to indicate the selected command
-//   activePadding: 0, // additional size in pixels for the active command
-//   indicatorSize: 25, // the size in pixels of the pointer to the active command, will default to the node size if the node size is smaller than the indicator size,
-//   separatorWidth: 5, // the empty spacing in pixels between successive commands
-//   spotlightPadding: 10, // extra spacing in pixels between the element and the spotlight
-//   adaptativeNodeSpotlightRadius: false, // specify whether the spotlight radius should adapt to the node size
-//   minSpotlightRadius: 10, // the minimum radius in pixels of the spotlight (ignored for the node if adaptativeNodeSpotlightRadius is enabled but still used for the edge & background)
-//   maxSpotlightRadius: 10, // the maximum radius in pixels of the spotlight (ignored for the node if adaptativeNodeSpotlightRadius is enabled but still used for the edge & background)
-//   openMenuEvents: "cxttap taphold", // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
-//   itemColor: "white", // the colour of text in the command's content
-//   itemTextShadowColor: "transparent", // the text shadow colour of the command's content
-//   zIndex: 9999, // the z-index of the ui div
-//   atMouse: false, // draw menu at mouse position
-//   outsideMenuCancel: 0, // if set to a number, this will cancel the command if the pointer is released outside of the spotlight, padded by the number given
-// };
-
-// const nodeCommands = [
-//   {
-//     content: '<i class="fas fa-edit"></i>',
-//     select: function (ele) {
-//       if (selected.length !== 0) {
-//         selected.forEach(function (node) {
-//           delete_nodes(node);
-//         });
-//         selected = [];
-//       } else {
-//         if (ele.data().type == 'atom') {
-//           delete_nodes(ele);
-//           ele.remove();
-//         } else if (ele.data().typeshape == 'diamond') {
-//           delete_nodes(ele);
-//           ele.remove();
-//         } else {
-//           sadface.delete_edge(ele.id());
-//           update_local_storage();
-//           ele.remove();
-//         }
-//       }
-//     },
-//     enabled: true,
-//   },
-// ];
-
-// const atomOptions = { ...cxtmenuOptions };
-// atomOptions.selector = 'node[type = "atom"]';
-// atomOptions.commands = [
-//   {
-//     content: '<i class="fas fa-edit"></i> content',
-//     select: function (ele) {
-//       $('#editContentModal').modal('show');
-//       $('#edit_atom_content').val(ele.data('content'));
-//       edit_atom = ele;
-//     },
-//     enabled: true,
-//   },
-//   {
-//     content: '<i class="fas fa-trash"></i> remove',
-//     select: function (ele) {
-//       $('#edit_metadata').empty();
-//       var atom = sadface.get_atom(ele.id());
-//       var textArea = $(
-//         '<textarea id="' +
-//           ele.id() +
-//           '_metadata" class="form-control" rows="2" >' +
-//           JSON.stringify(atom.metadata) +
-//           '</textarea>'
-//       );
-//       $('#edit_metadata').append(textArea);
-//       $('#editMetadataModal').modal('show');
-//       edit_atom = ele;
-//     },
-//     enabled: true,
-//   },
-//   ...nodeCommands,
-// ];
-
-// const atomOptions = { ...cxtmenuOptions };
-// atomOptions.selector = "node";
-// atomOptions.commands = [
-//   {
-//     content: <FontAwesomeIcon icon={faPlus} />,
-//     select: function (ele) {},
-//     enabled: true,
-//   },
-// ];
-
-// // @ts-ignore
-// cy.cxtmenu(atomOptions);
-
 type ElementKind = null | "atom" | "scheme" | "edge" | "graph";
 
 interface CtxMenuProps {
@@ -466,7 +364,6 @@ export default function Cytoscape() {
                 },
               ],
             });
-            cy?.center();
             updateGraph();
             handleClose();
           }}
