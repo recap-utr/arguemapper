253defe7db732ee3e3f54bb2f4c9f6594a5b2946
Mirko Lenz <mirko@mirkolenz.com>
2022-08-05 16:59:47 +0200

    Apply basic styles to atom and scheme nodes

--- src/components/NodeTypes.tsx
+++ src/components/NodeTypes.tsx
@@ -8,20 +8,29 @@ import {
 } from "react-flow-renderer";
 import * as model from "../model";
 
-const NodeType: ComponentType<NodeProps<model.NodeData>> = ({ data }) => {
+const AtomComponent: ComponentType<NodeProps<model.AtomData>> = (node) => {
   return (
-    <Box>
+    <Box sx={{ background: "blue" }}>
       <Handle type="source" position={Position.Top} />
-      <div>{model.nodeLabel(data)}</div>
+      <div>{model.nodeLabel(node)}</div>
+      <Handle type="target" position={Position.Bottom} />
+    </Box>
+  );
+};
+
+const SchemeComponent: ComponentType<NodeProps<model.SchemeData>> = (node) => {
+  return (
+    <Box sx={{ background: "green" }}>
+      <Handle type="source" position={Position.Top} />
+      <div>{model.nodeLabel(node)}</div>
       <Handle type="target" position={Position.Bottom} />
     </Box>
   );
 };
 
 const NodeTypes: FlowNodeTypes = {
-  input: NodeType,
-  default: NodeType,
-  output: NodeType,
+  atom: AtomComponent,
+  scheme: SchemeComponent,
 };
 
 export default NodeTypes;
