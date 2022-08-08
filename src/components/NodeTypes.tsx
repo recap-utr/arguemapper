3051a1647dc2e23753ff102667f2e593408d9032
Mirko Lenz <mirko@mirkolenz.com>
2022-08-08 09:53:18 +0200

    Use custom node and edge types

--- src/components/NodeTypes.tsx
+++ src/components/NodeTypes.tsx
@@ -1,30 +1,69 @@
-import { Box } from "@mui/material";
-import { ComponentType } from "react";
+import { Box, useTheme } from "@mui/material";
+import * as color from "@mui/material/colors";
+import React, { ComponentType } from "react";
 import {
   Handle,
   NodeProps,
   NodeTypes as FlowNodeTypes,
-  Position,
+  Position
 } from "react-flow-renderer";
 import * as model from "../model";
+import { useGraph } from "./GraphContext";
 
-const AtomComponent: ComponentType<NodeProps<model.AtomData>> = (node) => {
+interface NodeComponentProps extends React.PropsWithChildren {
+  node: NodeProps<model.NodeData>;
+  bg: string;
+}
+
+const NodeComponent: React.FC<NodeComponentProps> = ({
+  node,
+  bg,
+  children,
+}) => {
+  const theme = useTheme();
+
   return (
-    <Box sx={{ background: "blue" }}>
+    <Box sx={{ background: bg, padding: 1, borderRadius: 2 }}>
       <Handle type="source" position={Position.Top} />
-      <div>{model.nodeLabel(node)}</div>
+      {children}
       <Handle type="target" position={Position.Bottom} />
     </Box>
   );
 };
 
-const SchemeComponent: ComponentType<NodeProps<model.SchemeData>> = (node) => {
+const AtomComponent: ComponentType<NodeProps<model.AtomData>> = (node) => {
+  const body = <div>{model.nodeLabel(node)}</div>;
+  const { graph } = useGraph();
+  let bg: string = color.blue[500]
+
+  if (graph.majorClaim === node.id) {
+    bg = color.blue[900]
+  }
+
   return (
-    <Box sx={{ background: "green" }}>
-      <Handle type="source" position={Position.Top} />
-      <div>{model.nodeLabel(node)}</div>
-      <Handle type="target" position={Position.Bottom} />
-    </Box>
+    <NodeComponent node={node} bg={bg}>
+      {body}
+    </NodeComponent>
+  );
+};
+
+const SchemeComponent: ComponentType<NodeProps<model.SchemeData>> = (node) => {
+  const body = <div>{model.nodeLabel(node)}</div>;
+  let bg: string = color.teal[500];
+
+  if (node.data.scheme !== undefined) {
+    if (node.data.scheme.type === model.SchemeType.SUPPORT) {
+      bg = color.green[500];
+    }
+    else if (node.data.scheme.type === model.SchemeType.ATTACK) {
+      bg = color.red[500];
+    }
+  }
+
+  return (
+    <NodeComponent node={node} bg={bg}>
+      {body}
+    </NodeComponent>
   );
 };
 
