0a04e708ba7440821a6f19fd6aab5594e36d3b18
Mirko Lenz <mirko@mirkolenz.com>
2022-08-08 09:52:47 +0200

    Open in chrome upon launch

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+process.env.BROWSER = "chrome";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
   build: {
     target: "es2020",
   },
