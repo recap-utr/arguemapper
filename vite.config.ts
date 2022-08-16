2c1d7d65f6c8d0a48b131be6599610cf141fa427
Mirko Lenz <mirko@mirkolenz.com>
2022-08-16 20:27:17 +0200

    Open browser on start of vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 
-process.env.BROWSER = "chrome";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  server: {
-    open: true,
-  },
   build: {
     target: "es2020",
   },
