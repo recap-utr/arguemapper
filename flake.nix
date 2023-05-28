25ae095e3c0e7f5fed2f3e652b3ab831bb5fc44b
Mirko Lenz <mirko@mirkolenz.com>
2023-05-28 23:13:57 +0200

    build: migrate from devenv to mkshell

--- flake.nix
+++ flake.nix
@@ -3,22 +3,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = inputs@{ nixpkgs, flake-parts, devenv, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ devenv.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { pkgs, ... }: {
-        devenv.shells.default = {
-          enterShell = "npm install";
-          languages.javascript = {
-            enable = true;
-            package = pkgs.nodejs-18_x;
-          };
+        devShells.default = pkgs.mkShell {
+          shellHook = "npm install";
+          packages = with pkgs; [ nodejs-18_x ];
         };
       };
     };
