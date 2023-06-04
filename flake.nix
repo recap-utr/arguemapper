b6dc7419f1f06ab5bccb52cc56ce28efa783cb1e
Mirko Lenz <mirko@mirkolenz.com>
2023-06-04 22:26:42 +0200

    chore: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,22 @@
 # https://devenv.sh/reference/options/
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { pkgs, ... }: {
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {pkgs, ...}: {
         devShells.default = pkgs.mkShell {
           shellHook = "npm install";
-          packages = with pkgs; [ nodejs-18_x ];
+          packages = with pkgs; [nodejs-18_x];
         };
       };
     };
