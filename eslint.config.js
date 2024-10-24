174b59eb3b0b9a8a46e8da3d7104aa9bd6e52de0
Mirko Lenz <mirko@mirkolenz.com>
2024-10-24 22:22:14 +0200

    chore: switch to typescript-esline

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.js"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
