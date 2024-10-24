f7bd8930924f118a7c77b10e400a7ea01c9b10a8
Mirko Lenz <mirko@mirkolenz.com>
2024-10-24 23:58:44 +0200

    chore(eslint): remove typeChecked

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,8 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   {
     languageOptions: {
       parserOptions: {
